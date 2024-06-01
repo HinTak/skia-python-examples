376d05e1f8d54ed59c0ac89cff076bffed0b5387
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-06-01 11:17:25 +0100

    Adding glfw.window_hint(glfw.VISIBLE, glfw.FALSE) too

--- issue-214-glfw.py
+++ issue-214-glfw.py
@@ -28,6 +28,8 @@ glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
 glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 2)
 glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
 glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+#
+glfw.window_hint(glfw.VISIBLE, glfw.FALSE)
 window = glfw.create_window(640, 480, '', None, None)
 glfw.make_context_current(window)
 
