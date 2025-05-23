42aff4c31cf45002595c74be98e13565b99cbeb4
Hin-Tak Leung <htl10@users.sourceforge.net>
2025-05-23 01:18:24 +0100

    Another weird decision from Copilot
    It says QOpenGLWidget does not have aboutToBeDestroyed.
    It says it is in QGLWidget. It isn't - testing with pyqt5 version
    (glwidget_abouttobedestroyed.py) does not work.
    
    Okay, reading the comment again - it says it is in QGLWidget in PySide1 and PyQt4(!).

--- glwidget_abouttobedestroyed.py
+++ glwidget_abouttobedestroyed.py
@@ -1,22 +1,18 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtOpenGLWidgets import QOpenGLWidget
-from PyQt6.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtOpenGL import QGLWidget
+from PyQt5.QtCore import pyqtSlot
 import sys
 
-class MyOpenGLWidget(QOpenGLWidget):
+class MyOpenGLWidget(QGLWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         # Connect the aboutToBeDestroyed signal to your cleanup slot
-        #self.aboutToBeDestroyed.connect(self.cleanupGL)
+        self.aboutToBeDestroyed.connect(self.cleanupGL)
 
-    #@pyqtSlot()
-    #def cleanupGL(self):
-    #    # This will be called just before the GL context is destroyed
-    #    print("OpenGL context is about to be destroyed. Clean up GPU resources here.")
-
-    def closeEvent(self, event):
-        print("Widget is about to be destroyed")
-        super().closeEvent(event)
+    @pyqtSlot()
+    def cleanupGL(self):
+        # This will be called just before the GL context is destroyed
+        print("OpenGL context is about to be destroyed. Clean up GPU resources here.")
 
     def initializeGL(self):
         print("initializeGL: OpenGL context created.")
