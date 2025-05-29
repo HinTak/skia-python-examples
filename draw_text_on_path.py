c9ccfac8f2e4dd67eaed7cf1c26d02e4016c2b64
Hin-Tak Leung <htl10@users.sourceforge.net>
2025-05-29 17:23:23 +0100

    Comment on requirement

---
+++ draw_text_on_path.py
@@ -0,0 +1,40 @@
+# Requires at least skia-python m138.
+
+from skia import *
+from math import sin
+
+W = 800
+H = 400
+surface = Surface(W, H)
+canvas = surface.getCanvas();
+canvas.clear(ColorWHITE);
+
+myPath = Path()
+myPath.moveTo(60, 200)
+x = 60
+while (x <= 740):
+    y = 200 + 60 * sin(2 * 3.14159 * (x-60) / 350)
+    myPath.lineTo(x, y)
+    x += 10
+
+pathPaint = Paint()
+pathPaint.setAntiAlias(True)
+pathPaint.setStyle(Paint.kStroke_Style)
+pathPaint.setColor(ColorBLUE)
+pathPaint.setStrokeWidth(2)
+canvas.drawPath(myPath, pathPaint)
+
+font = Font(Typeface(""), 72)
+
+paint = Paint()
+paint.setAntiAlias(True)
+paint.setColor(ColorBLACK)
+
+m = Matrix.I()
+
+canvas.drawTextOnPath("Morphing Text On Path!", myPath, m, font, paint)
+
+canvas.flush()
+surface.flushAndSubmit()
+img = surface.makeImageSnapshot()
+img.save("morphed_text.png")
